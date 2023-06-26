eee3d45497e849f5af9b7f7c629a0debf5fbd4ad
Kim Hixson <khixson1@users.noreply.github.com>
2023-06-25 23:45:05 -0700

    Create script.js

---
+++ Web_Fundamentals/Javascript/buttonClicker/script.js
@@ -0,0 +1,11 @@
+// The function when called will change the displayed 
+//text in our button to 'Logout'
+function switchText(element) {
+    element.innerText = "Logout";
+}
+
+// The function when called will remove the displayed 
+//button from our webpage
+function removeButton(element) {
+    element.remove();
+}
